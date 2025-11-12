1f7d896ac70a7c68c13f8521d12b41b39babcaee
M.Ahmad Tariq <ahmadtariq4090@gmail.com>
2025-11-12 21:26:10 +0500

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,9 @@
 import pandas as pd
 from sklearn.datasets import load_iris
+import os
+
+os.makedirs("data", exist_ok=True)
+# Proceed with saving the file (e.g., df.to_csv("data/yourfile.csv"))
 
 iris = load_iris()
 data = pd.DataFrame(iris.data, columns=iris.feature_names)
